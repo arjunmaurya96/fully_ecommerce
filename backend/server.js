1f512086bf7b6deccea7bb69b3c2370c9e440a72
unknown <skm11794@gmail.com>
2025-01-30 18:33:46 +0530

    Remove secrets from commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("hello shubham ")
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import connectDB from "./config/db.js";
+//import apiKeyAuth from "./models/middleware/authMiddleware.js"; unComment and use
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: "all is ok" });
+});
+
+// 🔹 Apply Middleware Globally (all API Calls  Protected)
+//app.use(apiKeyAuth); crrently disable middelware when production then enable globale authhader unComment and use
+
+app.use("/auth", authRoutes);
+connectDB();
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
